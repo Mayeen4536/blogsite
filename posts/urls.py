5933d0a3665fda339b49305a63b528b616925d4b
Mayeen4536 <63101853+Mayeen4536@users.noreply.github.com>
2021-07-24 00:53:36 +0600

    added log in and log out buttons

--- posts/urls.py
+++ posts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<str:pk>', views.post, name='post'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -49,3 +49,9 @@ def login(request):
             return redirect('login')
     else:
         return render (request,'login.html')
+
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
